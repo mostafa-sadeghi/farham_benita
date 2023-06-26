2c0d27f57e67fe798feb0acc071d0ac116e8d97f
msa <msai.sadehi@gmail.com>
2023-06-26 16:58:57 +0330

    sadeghi

--- snake_game.py
+++ snake_game.py
@@ -1,12 +1,18 @@
 from turtle import Screen, Turtle
 from random import randint
-# TODO یک تابع بسازید که کارش ایجاد صفحه می باشد
+
+
 def make_screen():
-    pass
-main_surface = Screen()
-main_surface.bgcolor('black')
-main_surface.title("Snake Game")
-main_surface.setup(width=600, height=600)
+    window = Screen()
+    window.bgcolor('black')
+    window.title("Snake Game")
+    window.setup(width=600, height=600)
+    return window
+
+
+main_surface = make_screen()
+
+
 def make_turtle(tshape, tcolor):
     my_turtle = Turtle()
     my_turtle.shape(tshape)
@@ -14,12 +20,53 @@ def make_turtle(tshape, tcolor):
     my_turtle.penup()
     my_turtle.speed("fastest")
     return my_turtle
+
+
 def change_food_position():
-    pass
+    x = randint(-270, 270)
+    y = randint(-270, 270)
+    snake_food.goto(x,y)
+
+
 snake_head = make_turtle("square", "blue")
 
 snake_food = make_turtle("circle", "red")
+change_food_position()
+
 
 running = True
 while running:
     main_surface.update()
+
+
+"""
+TODO
+یک تابع بنویسید که لیستی از اعداد را به عنوان پارامتر بگیرد
+و 
+مجموع اعداد زوج موجود در آن را محاسبه کند و برگرداند
+numbers = [102,109,2,55,67,88]
+"""
+
+"""
+یک تابع بنویسید که لیستی از اعداد را از ورودی دریافت نماید و 
+در صورت صعودی بودن لیست 
+True 
+و در صورت نزولی بودن لیست
+False
+را برگرداند
+
+صعودی بودن یعنی هر عدد موجود در لیست از عدد قبلی بزرگتر باشد
+مثال
+[1,2,3,4,8]   نمونه از لیست صعودی
+
+
+نزولی:
+هر عدد از بعدی کوچکتر باشد
+
+[5,3,1,0]  نمونه یک لیست نزولی
+
+
+در تابع از 
+for   و   if
+استفاده کنید
+"""
