c43d82b689baf64401dc5ec47f9bc84d16bfe5a6
msa <msai.sadeghi@gmail.com>
2023-11-27 16:56:53 +0330

    s

--- pygame_courses/Dog_game/main.py
+++ pygame_courses/Dog_game/main.py
@@ -11,6 +11,11 @@ my_font = pygame.font.Font('assets/font.ttf',64)
 welcome_text = my_font.render('Welcome To My Game', True, (255,20,170))
 welcome_rect = welcome_text.get_rect(center = (SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
 
+
+dog_img = pygame.image.load("assets/dog.png")
+dog_rect = dog_img.get_rect(bottom = SCREEN_HEIGHT, centerx = SCREEN_WIDTH/2)
+
+
 running = True
 while running:
     for event in pygame.event.get():
@@ -20,5 +25,10 @@ while running:
     screen.fill((0,0,0))
     if pygame.time.get_ticks() - start_time < 3000:
         screen.blit(welcome_text, welcome_rect)
+    else:
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            dog_rect.x -= 5
+        screen.blit(dog_img, dog_rect)
     pygame.display.update()
     clock.tick(FPS)
